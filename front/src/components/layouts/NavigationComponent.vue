c43c7fded1203b6b1150d7b15a304e8708e47633
CORP\mahomsi <marwa.homsi@capgemini.com>
2023-10-18 10:02:32 +0200

    setup-front

---
+++ front/src/components/layouts/NavigationComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="nav-left">
+    <img class="icon" src="../../assets/logo.png" />
+    <h3>{{ title }}</h3>
+  </div>
+  <div class="nav-right">
+    <a>Bootcamps</a>
+    <a>Courses</a>
+    <a>About us</a>
+    <router-link to="/login"><a>Login</a></router-link>
+    <!-- <router-link to="/login">Login</router-link> -->
+  </div>
+</template>
+<script>
+export default {
+  props: ['title'],
+};
+</script>
+
+<style scoped>
+.icon {
+  padding-top: 0.3rem;
+  width: 4rem;
+}
+.nav-left {
+  display: flex;
+  margin-right: 20px;
+  flex-grow: 1;
+}
+.nav-left h3 {
+  margin-left: 1rem;
+  padding-top: 0.6rem;
+  color: white;
+}
+.nav-right {
+  display: flex;
+}
+a {
+  margin-right: 80px; /* Ajoute de l'espace entre les liens */
+  text-decoration: none; /* Supprime la décoration de lien par défaut */
+  color: white; /* Couleur du texte des liens (ajustez selon vos préférences) */
+}
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+import NavigationComponent from './components/layouts/NavigationComponent.vue';
+import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
+import AddBootcamp from './components/bootcamps/AddBootcamp.vue';
+import AllBootcamps from './components/bootcamps/AllBootcamps.vue';
+import LoginForm from './components/login/LoginForm.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/new-bootcamp', component: AddBootcamp },
+    { path: '/bootcamps', component: AllBootcamps },
+    { path: '/login', component: LoginForm },
+  ],
+});
+const app = createApp(App);
+
+app.component('navigation-component', NavigationComponent);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+
+app.use(router);
+app.mount('#app');
